283a3adb32e465c85b7e2011c9161846b4d07c38
AlbertoFerreiraC <108096405+AlbertoFerreiraC@users.noreply.github.com>
2022-09-23 20:11:20 -0400

    Siclo do while

---
+++ Ejercicio26.java
@@ -0,0 +1,48 @@
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Ejercicio26 {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader bufEntrada = new BufferedReader(new InputStreamReader(System.in));
+        int contador1;
+        int contador2;
+        int contador3;
+        int i;
+        int n;
+        int num[];
+        int x[];
+        Scanner sc = new Scanner(System.in);
+        int numero = 0;
+        do {
+            System.out.println("Ingrese un numero (99 para salir)");
+            n = Integer.parseInt(bufEntrada.readLine());
+            contador1 = 0;
+            contador2 = 0;
+            contador3 = 0;
+            x = new int[n];
+            num = new int[n];
+            for (i = 1; i <= n; i++) {
+                System.out.println("El elemento en la posicion " + i);
+                num[n - 1] = Integer.parseInt(bufEntrada.readLine());
+                if (num[n - 1] < 0) {
+                    contador1 = contador1 + 1;
+                }
+                if (num[n - 1] == 0) {
+                    contador2 = contador2 + 1;
+                }
+                if (num[n - 1] > 0) {
+                    contador3 = contador3 + 1;
+                }
+            }
+            System.out.println("La cantidad de elementos negativos del Vector X es: " + contador1);
+            System.out.println("La cantidad de elementos nulos del Vector X es: " + contador2);
+            System.out.println("La cantidad de elementos positivos del Vector X es: " + contador3);
+            numero = sc.nextInt();
+        } while (numero != 99);
+    }
+
+}
+
+
